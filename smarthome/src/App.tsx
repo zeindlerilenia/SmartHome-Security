592cf5a4a755a86295c79838386e428ba100d071
seungil05 <heusserd@bzz.ch>
2023-06-09 09:07:20 +0200

    Create function for loginPage and HomePage

--- smarthome/src/App.tsx
+++ smarthome/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
 
-function App() {
+export enum AuthState {
+  LoggedOut,
+  LoggedIn,
+}
+
+const App: React.FC = () => {
+  const [authState, setAuthState] = useState(AuthState.LoggedOut);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {authState === AuthState.LoggedOut ? (
+        <LoginPage onLogin={setAuthState} />
+      ) : (
+        <HomePage />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ smarthome/src/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Smart Home</h1>
+      <p>This is your smart home dashboard.</p>
+    </div>
+  );
+};
+
+export default HomePage;
+
