876fafa4c9111ea230798e5ead4b685ae612f718
mamba4l <ejla.maksuti@gmail.com>
2023-07-07 14:47:15 +0200

    modified import

--- smarthome/src/home/Components/Devices.tsx
+++ smarthome/src/home/Components/Devices.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
 
 const Devices: React.FC = () => {
   return (
